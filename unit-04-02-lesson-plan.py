42a64640407c5423d99b674a7ae4a9f3d528eb66
John Le <82713+sandbox@users.noreply.github.com>
2024-09-06 10:04:09 -0700

    Update unit-04-02-lesson-plan.py

--- unit-04-02-lesson-plan.py
+++ unit-04-02-lesson-plan.py
@@ -16,7 +16,11 @@ for i in range(0, 10):
 #   Range goes from the first number to the last number, but doesn't include the last number
 # What happens if the range is 5, 100?
 # What is the last number that gets printed?
-
+# 
+# Note that range can also take in a third value that is optional.
+#   the third value indicates the step size to take
+for i in range(0, 10, 2):
+  print(i)
   
 # Next example, Part 2
 a_list = ['apple', 'orange', 'pear', 'strawberry', 'grape']
@@ -48,4 +52,21 @@ for i in range(len(numbers)):
 # It's tricky to get the beginning and end of the traversal right. 
 # On the following page is a function that is supposed to compare two words and return True if one of the words is the reverse of the other.
 # The code will contain two common errors.
+#
+# This looks very tricky to explain
+# The common errors: off by one at the end of the list, off by one at the beginning of the list
+def is_reverse(word1, word2):
+  if len(word1) != len(word2):
+    return False
+  i = 0
+  j = len(word2)
+  while j > 0:
+    if word1[i] != word2[j]:
+      return False
+    i = i + 1
+    j = j - 1
+return True
+print(is_reverse('pots', 'stop'))
+
+
 
