f8be525f7a9120f45b242ef77cbf4e5efb31d2fb
John Le <moonpiesandbox@gmail.com>
2024-09-06 10:15:16 -0700

    Updated unit-04-02-lesson-plan.py

--- unit-04-02-lesson-plan.py
+++ unit-04-02-lesson-plan.py
@@ -5,23 +5,23 @@
 # After this lesson, you will be able to...
 #   Define and identify for loop, item, iteration, scope
 #   Loop through (traverse) the items in a list
-#   Be aware of the scope of variables during iteration 
+#   Be aware of the scope of variables during iteration
 #
 # Type the following (copy the following)
 # Then run the code and talk about what happened
-for i in range(0, 10): 
+for i in range(0, 10):
   print(i)
 
 # What does the range function do? Write it down
 #   Range goes from the first number to the last number, but doesn't include the last number
 # What happens if the range is 5, 100?
 # What is the last number that gets printed?
-# 
+#
 # Note that range can also take in a third value that is optional.
 #   the third value indicates the step size to take
 for i in range(0, 10, 2):
   print(i)
-  
+
 # Next example, Part 2
 a_list = ['apple', 'orange', 'pear', 'strawberry', 'grape']
 print(len(a_list))
@@ -36,11 +36,11 @@ print(list(range(0, len(a_list)))
 # Part 3: Lets try using the range() and len() functions to make a for loop that prints the elements of a_list, one at a time.
 # Answer:
 for i in range(0, len(a_list)):
-  print(a_list[i])  
+  print(a_list[i])
 
 
-# Most common way to traverse the elements of a list is with a for loop. 
-# To write or update the elements, use indices. 
+# Most common way to traverse the elements of a list is with a for loop.
+# To write or update the elements, use indices.
 # A common way to do that is to combine the functions range() and len():
 #
 # Example
@@ -52,7 +52,7 @@ for i in range(len(numbers)):
 
 # Debugging
 #
-# It's tricky to get the beginning and end of the traversal right. 
+# It's tricky to get the beginning and end of the traversal right.
 # On the following page is a function that is supposed to compare two words and return True if one of the words is the reverse of the other.
 # The code will contain two common errors.
 #
@@ -78,8 +78,35 @@ print(is_reverse('pots', 'stop'))
 
 # Lab fruit_pluralizer
 #
+'''
+Write a function pluralize_words that takes in a list of words and updates the values of the list to make each one plural. It returns nothing.
 
-# Lab Reverse String 
+Making plurals in English has a number of special cases, but for this lab we'll use a simple rule: if the word ends in a y remove the y and add ies; otherwise add an 's'.
+
+We'll exercise the function on a list of words.
+
+Create the function contract for pluralize_words.
+Provide a few examples that confirm pluralize_words works as expected:
+Include examples with 'berry'
+What if the list is empty?
+
+Here is what it should look like when you run your code
+Singular words: ['apple', 'berry', 'melon']
+No longer singular words: ['apples', 'berries', 'melons']
+'''
+
+# contract goes here
+def pluralize_words(word_list):
+    # your code goes here
+
+word_list = ['apple', 'berry', 'melon']
+print("Singular words: " + str(word_list))
+pluralize_words(word_list)
+print("No longer singular words: " + str(word_list))
+# more examples go here
+
+
+# Lab Reverse String
 #
 '''
 Create a function my_reverse, which will return a reversed string.
@@ -95,6 +122,6 @@ A string of length 1
 def my_reverse(string_to_reverse):
     # your code goes here
 
-reversed = my_reverse("apples")
-print(reversed)
+reversed_string = my_reverse("apples")
+print(reversed_string)
 # examples go here
