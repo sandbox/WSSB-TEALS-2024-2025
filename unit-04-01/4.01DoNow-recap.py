24a31a6c12d8f4b0e6a92e1325d25b94cd714412
John Le <moonpiesandbox@gmail.com>
2024-09-09 10:47:56 -0700

    Adding recap and de_vowel lab

---
+++ unit-04-01/4.01DoNow-recap.py
@@ -0,0 +1,24 @@
+# Recap Day 1
+#
+# Formal Definition Time
+#
+#   A loop is a sequence of instructions that is continually repeated until a certain condition is reached.
+#
+#   Each repetition is called an iteration.
+#
+# What was the example we did yesterday using the for loop?
+# What did using a for loop help us accomplish?
+#   Remember how much code it took to add an 's' to each fruit in our
+#   multi_fruit when we didn't have a for loop?
+#
+# Finish writing the for loop and adding to our multi_fruit list
+
+
+# Another for loop example
+#
+# We can use the for loop to search through a list and find
+# the item we're interested in
+#
+# Make a list of animals
+# Find the animal in your list that starts with your favorite letter
+# Print the animal you found
---
+++ unit-04-01/4.01Lab-de_vowel.py
@@ -0,0 +1,27 @@
+# Lab Activity
+#
+# Create a de_vowel function.
+#    Input is a string
+#    Output is a copy of the string
+#
+# The copy should have the vowels removed
+# 'a', 'e', 'i', 'o', and 'u' are counted as vowels, but not 'y'.
+#
+# Create the function contract for de_vowel.
+#
+# Write de_vowel using a for loop
+#
+# Provide a few examples that confirm de_vowel works as expected:
+#   What if the string is all vowels?
+#   What if there are no vowels?
+#   What if there is a mix of vowels and non-vowels and spaces?
+#   What if some of the vowel characters are capitalized?
+#
+# Starter Code
+
+# contract goes here
+def de_vowel(a_string):
+# your code goes here
+no_vowels = de_vowel("This sentence has no vowels")
+print(no_vowels)
+# examples go here
