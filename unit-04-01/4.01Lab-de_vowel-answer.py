81b494111892aa91bd567751d7187847f2b3cb1a
John Le <moonpiesandbox@gmail.com>
2024-09-10 10:10:29 -0700

    updated examples and made case sensitive

--- unit-04-01/4.01Lab-de_vowel-answer.py
+++ unit-04-01/4.01Lab-de_vowel-answer.py
@@ -19,15 +19,32 @@
 #
 # Starter Code
 
-# contract goes here
+# Name de_vowel
+# Purpose remove all occurences of vowels aeiou from a string
+# Input a string
+# Output a string with all vowels removed
 def de_vowel(a_string):
   ans = ''
   for c in a_string:
-    if c not in ['a','e','i','o','u']:
+    if c.lower() not in 'aeiou':
       ans += c
+  return ans
+
+# longer example
+def de_vowel(a_string):
+  ans = ''
+  for x in a_string:
+    c = x.lower()
+    if c != 'a' and c != 'e' and c != 'i' and c != 'o' and c != 'u':
+      ans += x
   return ans
 
 # your code goes here
 no_vowels = de_vowel("This sentence has no vowels")
 print(no_vowels)
 # examples go here
+print(de_vowel('aeuio'))
+print(de_vowel('CCCCCCC'))
+print(de_vowel(''))
+print(de_vowel('no vowels'))
+print(de_vowel('Capitalized VOWeLS'))
