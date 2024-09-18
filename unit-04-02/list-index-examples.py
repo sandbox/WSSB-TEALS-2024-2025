1a7403b8280254be4ba92f9c036ea2a0d36e3755
John Le <moonpiesandbox@gmail.com>
2024-09-18 14:39:47 -0700

    list index examples for practice with lab 4.02

---
+++ unit-04-02/list-index-examples.py
@@ -0,0 +1,38 @@
+fruits = ['apple', 'berry', 'cherry', 'yuzu', 'orange']
+
+# Practice: update the second element of our fruits list to be 'berrygood'
+
+
+
+
+# Practice: grab the third element of the fruits list and use slicing
+# to print it without it's last letter
+
+
+
+
+
+# Example:
+#
+#   When we do a for loop over the elements of a list, we can't update
+#   the list we can only use it
+for element in fruits:
+  print(element)
+
+# if we try to update the element in this for loop, it updates the
+# element variable, but it does not update the element inside the
+# list. This is a tricky concept!
+for element in fruits:
+  # We're updating the element variable, but it doesn't change the element inside the list
+  element += 's'
+  print(element)
+print(fruits)
+
+# If we need to update the element of the list inside a for loop, we
+# need to do a for loop over the indexes of the list. That uses the
+# range and len functions, and then we can use the index to then access
+# element in the list at that index, like in the following:
+for index in range(0, len(fruits)):
+  print(fruits[index]) # we're accessing the element of fruits at `index`
+  # Next, we're updating the element of fruits at `index`
+  fruits[index] += 's'
