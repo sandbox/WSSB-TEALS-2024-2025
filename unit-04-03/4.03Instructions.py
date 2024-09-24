c46e99e58b5fe986648942e704502a0a44335c4b
Candice Newton <penguininja@users.noreply.github.com>
2024-09-24 12:10:35 +0200

    Add 4.03 Lab Instructions

---
+++ unit-04-03/4.03Instructions.py
@@ -0,0 +1,83 @@
+# 4.03 Lab Instructions
+
+# In this lab we will be drawing images using nested for loops.
+
+# For each of the following problems, you will write a function 
+# that will draw the desired output. You may use an extra function 
+# if you find it helpful.
+
+# Part 1
+# Write a function, draw_7, to draw the 7×7 square (shown below)
+#     * * * * * * *
+#     * * * * * * *
+#     * * * * * * *
+#     * * * * * * *
+#     * * * * * * *
+#     * * * * * * *
+#     * * * * * * *
+
+# Part 2
+# Write a function stars_and_stripes, that will draw a 3 sets of rows. 
+# 1st a row of 7 stars followed by a row of 7 dashes (shown below)
+#     * * * * * * *
+#     - - - - - - -
+#     * * * * * * *
+#     - - - - - - -
+#     * * * * * * *
+#     - - - - - - -
+
+# Part 3
+# Write a function, increasing_triangle that will print out the following:
+#     1
+#     1 2
+#     1 2 3
+#     1 2 3 4
+#     1 2 3 4 5
+#     1 2 3 4 5 6
+#     1 2 3 4 5 6 7
+
+# Part 4
+# Write a function, vertical_stars_and_stripes that will print out the following:
+#     - * - * - * -
+#     - * - * - * -
+#     - * - * - * -
+#     - * - * - * -
+#     - * - * - * -
+#     - * - * - * -
+#     - * - * - * -
+
+# Part 5: Bonus
+# Use a function to create your own pattern or drawing. 
+# Some possible pattern ideas:
+
+# Write a function that will print a border around a 7×7 square (shown below)
+#     * * * * * * * *
+#     * - - - - - - *
+#     * - - - - - - *
+#     * - - - - - - *
+#     * - - - - - - *
+#     * - - - - - - *
+#     * - - - - - - *
+#     * - - - - - - *
+#     * * * * * * * *
+
+# Write a function that will print the following balanced_triangle.
+#     1
+#     1 2
+#     1 2 3
+#     1 2 3 4
+#     1 2 3 4 5
+#     1 2 3 4 5 6
+#     1 2 3 4 5 6 7
+#     1 2 3 4 5 6
+#     1 2 3 4 5
+#     1 2 3 4
+#     1 2 3
+#     1 2
+#     1
+
+# Write a function that will print the following triangle.
+#        *
+#       ***
+#      *****
+
