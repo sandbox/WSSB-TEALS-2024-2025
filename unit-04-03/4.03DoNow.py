5b52be32046597dc8216da44e5f43cdac62280c1
Candice Newton <penguininja@users.noreply.github.com>
2024-09-24 11:37:14 +0200

    Add section labels

--- unit-04-03/4.03DoNow.py
+++ unit-04-03/4.03DoNow.py
@@ -1,3 +1,4 @@
+# Part 1
 # Predict what will happen when you run the following code:
 
 def print_6_stars():
@@ -11,6 +12,7 @@ print_6_stars()
 # Run the code, and write down what the output of the 
 # function print_6_stars is. 
 
+# Part 2
 # Write a function print_star_square that calls 
 # print_6_stars in a loop to produce the following output.
 
@@ -21,6 +23,7 @@ print_6_stars()
 #    * * * * * *
 #    * * * * * *
 
+# Part 3
 # Rewrite the function print_star_square without using print_6_stars.
 # Note that there are two ways (at least) to get the above output, 
 # so just try doing both!
