5bdda1510e434490854f61435055003e7a254573
Candice Newton <penguininja@users.noreply.github.com>
2024-09-24 11:19:52 +0200

    Add instructions and second example to Do Now 4.03

--- unit-04-03/4.03DoNow.py
+++ unit-04-03/4.03DoNow.py
@@ -1,5 +1,24 @@
+# Run the following code:
+
 def print_6_stars():
     my_string = ''
     for i in range(0, 6):
         my_string += ' *'
-    print(my_string)
+    print(my_string)        
+
+# In your notebook, write down what the output of the 
+# function print_6_stars is. 
+
+# Write a function print_star_square that calls 
+# print_6_stars in a loop to produce the following output.
+
+#    * * * * * *
+#    * * * * * *
+#    * * * * * *
+#    * * * * * *
+#    * * * * * *
+#    * * * * * *
+
+# Rewrite the function print_star_square without using print_6_stars.
+# Note that there are two ways (at least) to get the above output, 
+# so just try doing both!
