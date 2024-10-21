62fc640fc149c1835dc97943698f1164234f7794
Candice Newton <penguininja@users.noreply.github.com>
2024-10-21 13:44:40 -0700

    Create 4.04DoNow.py

---
+++ unit-04-04/4.04DoNow.py
@@ -0,0 +1,15 @@
+# Type and run the following code
+
+# my_building is a representation of the apartments on 
+# each floor of my 3 story building
+
+my_building = [
+    ['apt1a', 'apt1b', 'apt1c'],
+    ['apt2a', 'apt2b', 'apt2c'],
+    ['apt3a', 'apt3b', 'apt3c']
+]
+print("first floor: " + str(my_building[0]))
+print("first floor, 3rd apartment: " + str(my_building[0][2]))
+
+# Write down what was printed.
+# How you would access the 2nd apartment of the 3rd floor (apt3b)?
