cb9c81f6a280f18abcf5f0ed1a98f68f47a3c996
Candice Newton <penguininja@users.noreply.github.com>
2024-10-23 13:11:11 -0700

    Add part 2 of DoNow

--- unit-04-04/4.04DoNow.py
+++ unit-04-04/4.04DoNow.py
@@ -4,12 +4,31 @@
 # each floor of my 3 story building
 
 my_building = [
-    ['apt1a', 'apt1b', 'apt1c'],
-    ['apt2a', 'apt2b', 'apt2c'],
-    ['apt3a', 'apt3b', 'apt3c']
+  ['apt1a', 'apt1b', 'apt1c'],
+  ['apt2a', 'apt2b', 'apt2c'],
+  ['apt3a', 'apt3b', 'apt3c']
 ]
 print("first floor: " + str(my_building[0]))
 print("first floor, 3rd apartment: " + str(my_building[0][2]))
 
 # Write down what was printed.
+
+# first floor: ['apt1a', 'apt1b', 'apt1c']
+# first floor, 3rd apartment: apt1c
+
 # How you would access the 2nd apartment of the 3rd floor (apt3b)?
+
+
+## Part 2
+
+# 1. Print all the apartments one at a time
+
+# 2. How would you show all apartments in a single list?
+
+# 3. Change all "c" apartments to say "suite"
+
+# 4. Print all the apartments that don't end in 'a'
+
+# 5. Add "(newly renovated!)" to all apartment names
+
+# Bonus: Add a "d" apartment to each list using the same naming convention
