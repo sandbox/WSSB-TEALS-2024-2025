b1cd1656a57bc1a155e5ce066edd050526d2038d
Candice Newton <penguininja@users.noreply.github.com>
2024-11-21 09:56:55 -0800

    Adding practice quiz

---
+++ unit-04-05/unit-04-quiz-practice.py
@@ -0,0 +1,18 @@
+# Do Now 4.05 Quiz Practice
+
+## Example 1: Write a function that prints numbers from 1 to 10.
+## Bonus: Make the numbers all appear on a single line
+
+## Example 2: Given a list of numbers, write a script to find 
+## and print the largest number.
+nums = [3, 7, 2, 9]
+
+## Example 3: Write a function that prints a 6x6 square of alternating rows
+## of stars and dashes
+
+## Example 4: Write a function that takes a string as an argument and 
+## searches a list for that string. The function should print the index
+## of the found item
+words = ['banana', 'apple', 'orange', 'grapes', 'yuzu']
+
+# find_string('grapes')
