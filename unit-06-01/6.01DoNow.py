805e2ddfbeee31ae1b81d034fbac44aadfa17784
Candice Newton <candice.c.newton@gmail.com>
2025-02-23 14:24:30 -0800

    Add 6.01 do now

---
+++ unit-06-01/6.01DoNow.py
@@ -0,0 +1,12 @@
+my_dictionary = {
+    'cat': 'a domestic feline',
+    'dog': 'a domestic canine',
+    'chair': 'furniture piece for sitting',
+    'car': 'automobile'
+}
+
+print(my_dictionary)
+print(my_dictionary['dog'])
+print(my_dictionary.get('dog'))
+print('cat' in my_dictionary)
+print('monkey' in my_dictionary)
