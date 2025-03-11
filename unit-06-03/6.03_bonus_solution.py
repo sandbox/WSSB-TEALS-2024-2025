424d7aa3cb9b25663c04fcbf69f1aa1370acd8bf
John Le <le.johnq@gmail.com>
2025-03-11 16:06:44 -0700

    bonuses

--- unit-06-03/6.03_bonus_solution.py
+++ unit-06-03/6.03_bonus_solution.py
@@ -4,13 +4,24 @@ todos = {
   k:[] for k in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 }
 
+def does_todo_already_exist(todo):
+  for day, day_todos in todos.items():
+    if todo in day_todos:
+      return True, day
+  return False, None
+
 while True:
   command = input("What would you like to do (format: action day item)? ")
   action, extra = command.split(' ', 1)
   if action == 'add':
     day, item = extra.split(' ', 1)
     if day in todos:
-      todos[day].append(item)
+      existence, which_day = does_todo_already_exist(item)
+      if existence:
+        print("'" + item + "' already exists in todo list for " + which_day + ".")
+      else:
+        print("Adding '" + item + "' to todo list for day " + day + ".")
+        todos[day].append(item)
     else:
       print("'" + day + "' is not a valid day to add to.")
   elif action == 'get':
