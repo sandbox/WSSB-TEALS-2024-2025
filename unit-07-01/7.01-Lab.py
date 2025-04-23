aa96e6fa79b9e6d3c801d1610353aafc11d2b7a4
John Le <82713+sandbox@users.noreply.github.com>
2025-04-23 00:32:38 -0400

    Create 7.01-Lab.py

---
+++ unit-07-01/7.01-Lab.py
@@ -0,0 +1,6 @@
+### Instructions
+# Create a class, Color.
+# Instantiate at least 3 colors.
+# Add attributes of r, g, and b to those instances.
+# Create a function, add_color, which takes in two colors and returns a color that is the sum of the two reds, greens, and blues.
+# Don't forget: the maximum value for R, G, or B is 255.
