784f34dfe02b7ab525ceeed5cb1d4917c277ad19
John Le <82713+sandbox@users.noreply.github.com>
2025-04-23 00:48:15 -0400

    Update 7.01-Lab-solution.py

--- unit-07-01/7.01-Lab-solution.py
+++ unit-07-01/7.01-Lab-solution.py
@@ -11,6 +11,11 @@ b.r = 191
 b.g = 10
 b.b = 255
 
+c = Color()
+c.r = 19
+c.g = 112
+c.b = 11
+
 def add_colors(c1, c2):
     color = Color()
     color.r = min(c1.r + c2.r, 255)
@@ -19,5 +24,8 @@ def add_colors(c1, c2):
 
     return color
 
-c = add_colors(a, b)
-print("My new color is", c.r, c.g, c.b)
+ab = add_colors(a, b)
+bc = add_colors(b, c)
+
+print("My new color ab is", ab.r, ab.g, ab.b)
+print("My new color bc is", bc.r, bc.g, bc.b)
