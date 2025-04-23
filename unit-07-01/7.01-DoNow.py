1fb8c34075dc59fe634362b65d7caf1e73eb3fec
John Le <82713+sandbox@users.noreply.github.com>
2025-04-23 00:28:41 -0400

    Create 7.01-DoNow.py

---
+++ unit-07-01/7.01-DoNow.py
@@ -0,0 +1,19 @@
+my_pet_1 = 'pet'
+my_pet_1_type = 'cat'
+my_pet_1_noise = 'meow'
+my_pet_1_full_name = 'Snuffles McGruff'
+
+my_pet_2 = 'pet'
+my_pet_2_type = 'cat'
+my_pet_2_noise = 'meow'
+my_pet_2_full_name = 'Snowpounce Flury'
+
+my_pet_3 = 'pet'
+my_pet_3_type = 'cat'
+my_pet_3_noise = 'meow'
+my_pet_3_full_name = 'Snickers Snorkel'
+
+my_pets = [my_pet_1, my_pet_2, my_pet_3]
+
+for pet in my_pets:
+    # print full name of each pet
