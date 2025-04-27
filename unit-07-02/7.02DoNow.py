383569259f6ce82cdbe16a9c2eb70f42c312b8b1
Candice Newton <candice.c.newton@gmail.com>
2025-04-27 14:10:27 -0700

    Adding 7.02 Do Now and Lab solution

---
+++ unit-07-02/7.02DoNow.py
@@ -0,0 +1,6 @@
+class Pet:
+  def __init__(self, name):
+    self.name = name
+
+my_pet = Pet('Peter', 'brown')
+print(my_pet.name)
---
+++ unit-07-02/7.02Lab.py
@@ -0,0 +1,21 @@
+class Pet:
+    """Defines pets and their attributes"""
+    def __init__(self, animal, color, food, noise, name):
+        self.animal = animal
+        self.color = color
+        self.food = food
+        self.noise = noise
+        self.name = name
+
+pet_1 = Pet('dog', 'brown', 'cheese', 'bark', 'Lassie')
+pet_2 = Pet('cat', 'black', 'pizza', 'meow', 'Kitty')
+pet_3 = Pet('mouse', 'white', 'seeds', 'squeak', 'Mike')
+
+pets = [pet_1, pet_2, pet_3]
+
+def pet_name_and_food(list_of_pets):
+    for pet in pets:
+        print(pet.name)
+        print(pet.food)
+
+pet_name_and_food(pets)
