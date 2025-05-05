ba2efe7845e5763c774f4c581ad068252de8d1e6
Candice Newton <candice.c.newton@gmail.com>
2025-05-05 09:18:56 -0700

    Add proposal requirements

---
+++ unit-08-01/FinalProject_proposal-requirements.py
@@ -0,0 +1,24 @@
+##
+## Proposal Requirements
+##
+
+# Project Title
+## A clear, descriptive name for the project
+
+# Project Overview
+## A brief description (2-3 sentences) explaining what the project is and what it will do
+
+# Target Users
+## Who will use this program and why
+
+# Learning Goals
+## What the student hopes to learn from creating this project
+
+# Technical Components
+## Which programming concepts from class will be incorporated (loops, conditionals, etc.)
+
+# Timeline
+## A basic breakdown of when different parts will be completed
+
+# Resources Needed
+## Any special libraries, tools, or assistance required
