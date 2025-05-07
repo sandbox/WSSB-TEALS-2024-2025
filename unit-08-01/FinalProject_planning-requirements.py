4d9d50d92bf469d27bb14581b1ecca005587953a
Candice Newton <penguininja@users.noreply.github.com>
2025-05-07 09:26:28 -0700

    Create FinalProject_planning-requirements.py

---
+++ unit-08-01/FinalProject_planning-requirements.py
@@ -0,0 +1,32 @@
+##
+## Project Planning Document Requirements
+##
+
+# Project Title and Description
+## Expanded from the proposal
+
+# Functional Requirements
+## Detailed list of what the program must do
+## Example output of the program, sample user interaction
+
+# Technical Requirements
+## Required variables and their purposes
+## Required functions with inputs/outputs
+## Data structures (lists, dictionaries) to be used
+## Required loops and conditionals
+## How nested structures will be implemented
+
+# Pseudocode Outline
+## A step-by-step outline of program logic
+
+# Testing Plan
+## How the student will test their program works as intended
+
+# Timeline
+## A basic breakdown of when different parts will be completed
+
+# Stretch Goals
+## Optional additional features if time permits
+
+# Final Submission Checklist
+## What to include in the final submission
